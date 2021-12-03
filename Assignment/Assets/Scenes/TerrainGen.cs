1397e01fe4dcc1b91e6a232f3f9b3e43b02c3f14
c18441084 <c18441084@mytudublin.ie>
2021-12-03 17:51:35 +0000

    adding

--- Assignment/Assets/Scenes/TerrainGen.cs
+++ Assignment/Assets/Scenes/TerrainGen.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TerrainGen : MonoBehaviour
 {
   public int counter = 0;
-  public float i = 256;
+  public float i = 244;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +15,17 @@ public class TerrainGen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if(GameObject.Find("Bus").transform.position.z>(244)){
+      if(GameObject.Find("Bus").transform.position.z>(i)){
         //GameObject.Find("Bus").transform.position = new Vector3(127, 37, 11);
         if(counter == 0){
-          GameObject.Find("Dessert").transform.position = new Vector3(127, 19, 244);
+          GameObject.Find("Dessert").transform.position = new Vector3(0, 18, i);
           i = i + 244;
+          counter = 1;
+        }
+        else if(counter == 1){
+          GameObject.Find("Mountain Range").transform.position = new Vector3(0, 0, i);
+          i = i + 244;
+          counter = 0;
         }
       }
     }
