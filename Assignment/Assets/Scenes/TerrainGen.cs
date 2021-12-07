08566a0413f6211139107c9f20087c6b1a3af15f
c18441084 <c18441084@mytudublin.ie>
2021-12-07 11:53:21 +0000

    Add

--- Assignment/Assets/Scenes/TerrainGen.cs
+++ Assignment/Assets/Scenes/TerrainGen.cs
@@ -28,7 +28,7 @@ public class TerrainGen : MonoBehaviour
           RenderSettings.skybox = Dessert_sky;
           GameObject.Find("Mountain").transform.position = new Vector3(0, -1000, i);
           i = i + 235;
-          GameObject.Find("Lorry").transform.position = new Vector3(132, 4, i-70);
+          GameObject.Find("Lorry").transform.position = new Vector3(132, 5, i-70);
           GameObject.Find("Lorry").transform.rotation = Quaternion.Euler(0, 180, 0);
           counter = 1;
         }
