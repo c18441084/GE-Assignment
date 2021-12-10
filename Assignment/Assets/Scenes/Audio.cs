e7e8b0f64b000393399dee5579316caff8590626
c18441084 <c18441084@mytudublin.ie>
2021-12-10 11:48:35 +0000

    ADd

--- Assignment/Assets/Scenes/Audio.cs
+++ Assignment/Assets/Scenes/Audio.cs
@@ -8,17 +8,19 @@ public class Audio : MonoBehaviour
     public AudioClip horn;
     public AudioSource audio;
     public float scale = 0.7f;
-    // Start is called before the first frame update
+    
     void Start()
     {
       audio.Play();
     }
 
-    void onCollosionEnter(Collision collision)
+    void onTriggerEnter(Collider collision)
     {
       if(collision.gameObject.name == "Car")
       {
-        audio.PlayOneShot(crash, scale);
+        Debug.Log("Entered");
+        audio.clip = crash;
+        audio.Play();
       }
       if(collision.gameObject.tag == "Lorry")
       {
